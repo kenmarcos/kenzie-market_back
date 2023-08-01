b1c942d29a0a7467abdb92e99940f5948063dc95
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-08-01 09:00:24 -0300

    chore: adjust env variables settings

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 const devEnv = {
   type: "postgres",
-  url: "postgresql://postgres:kenziemarket_pass@localhost:5432/kenziemarket_db",
+  host: "localhost",
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ["./src/entities/**/*.ts"],
   migrations: ["./src/database/migrations/*.ts"],
   cli: {
@@ -9,18 +13,19 @@ const devEnv = {
   logging: true,
 };
 
-const prodEnv = {
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  entities: ["./dist/entities/**/*.js"],
-  migrations: ["./dist/database/migrations/*.js"],
-  cli: {
-    migrationsDir: "./dist/database/migrations",
-  },
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-};
+// const prodEnv = {
+//   type: "postgres",
+//   url: process.env.DATABASE_URL,
+//   entities: ["./dist/entities/**/*.js"],
+//   migrations: ["./dist/database/migrations/*.js"],
+//   cli: {
+//     migrationsDir: "./dist/database/migrations",
+//   },
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : false,
+// };
 
-module.exports = process.env.NODE_ENV === "production" ? prodEnv : devEnv;
+// module.exports = process.env.NODE_ENV === "production" ? prodEnv : devEnv;
+module.exports = devEnv;
